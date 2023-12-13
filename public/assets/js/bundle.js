4a2e917a57a892b81cfc787c6d71ecf674df6bf5
jon_prog <jonashenri.aquino@gmail.com>
2023-12-13 04:47:48 -0300

    concluded study about webpack

---
+++ public/assets/js/bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!***********************!*\
+  !*** ./src/script.js ***!
+  \***********************/
+var _;
+var a = (_ = 3) !== null && _ !== void 0 ? _ : 10;
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
